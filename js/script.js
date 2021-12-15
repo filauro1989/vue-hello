cdd3703dd43ea93618d87a47f52ec8ba35e33967
filauro1989 <filauro1989@gmail.com>
2021-12-15 13:39:01 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// Bonus:
+// In una cartella bonus:
+// Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
+
+const container = new Vue(
+    {
+        el: '#container',
+        data: {
+            message: 'lorem ipsum e anche un pò di più!',
+            image: 'https://www.shutterstock.com/image-vector/set-nine-300x300-valentines-day-banner-94332646'
+        },
+    }
+);
